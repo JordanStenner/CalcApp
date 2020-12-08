2430b01373e3c82a7c3ba16e3993c61c6269e2bc
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-12-08 14:48:23 +0000

    Added integration testing for add numbers

--- routes.js
+++ routes.js
@@ -10,4 +10,12 @@ function coinFlipRoute(request, response) {
   }
 }
 
+function addNumbersRoute(request, response){
+  let num1 = Number(request.params.num1);
+  let num2 = Number(request.params.num2);
+
+  response.send(logic.addNumbers(num1, num2).toString());
+}
+
 module.exports.coinFlipRoute = coinFlipRoute;
+module.exports.addNumbersRoute = addNumbersRoute;
--- server.js
+++ server.js
@@ -4,5 +4,6 @@ let routes = require("./routes");
 let app = express();
 
 app.get("/flip", routes.coinFlipRoute);
+app.get("/add/:num1/:num2", routes.addNumbersRoute);
 
 module.exports.app = app;
